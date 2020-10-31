ce5856950c757062f60a81703da0a52810772ea4
Kirill <peacedolay322@gmail.com>
2020-11-01 00:21:06 +0300

    тест
    добавил, удалил строчку, прикольно

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,7 +7,6 @@ public class Main {
 
         Simulation simulation = new Simulation(10,10,17,3,800,1000,50,100,50,100);
         simulation.start(10000);
-        System.out.println(simulation.getIterFinal());
         System.out.println(simulation.getAmountZd()+" "+simulation.getAmountInf()+" "+simulation.getAmountVzd());
 
     }

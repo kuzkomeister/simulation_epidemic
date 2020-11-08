a38448626ce936b1245742463e7b6ec9adcb2d62
Kirill <peacedolay322@gmail.com>
2020-11-08 21:42:32 +0300

    Текущая модель из ТЗ
    Реализовано все кроме ТЗ на 08.11.2020, кроме распределения масок при инициализации симуляции

--- src/main/Man.java
+++ src/main/Man.java
@@ -2,38 +2,58 @@ package main;
 
 public class Man {
     //=====
-    // 0 - здоровый, 1 - инфицированный, 2 - выздоревевший
+    // 0 - здоровый, 1 - инфицированный в инкубационном периоде,
+    // 2 - инфицированный в клиническом периоде, 3 - выздоровевший, 4 - мертвый
+    // 5 - инфицированный без проявления симптомов
     private byte condition;         // Текущее состояние человека
     private float x,y;              // Координаты человека на карте
     private float[] vectorDirection;// Вектор направления человека
-    //===== Время
+
+    boolean mask;       // Надета маска
+    boolean socDist;    // Соблюдает социальное дистанцирование
+    boolean infectHand; // Инфицированы ли руки
+    //===== Текущее время
     private int timeRecovery;       // время необходимое для выздоровления
+    private int timeInfInc;         // время инкубационного периода
     private int timeSneeze;         // время между чихами
-    private int timeChangeDirect;   // время между сменой направлений
+    private int timeHandToFaceContact;// время между контактом рук с лицом
+    private int timeWash;           // время между мытьем/дезинфекцией рук
+    private int timeChangeDirect = 1;   // время между сменой направлений
+
     //===== Вероятности
-    private float probabilityInfection;     // Текущая вероятность удачной попытки заразить
-    private float probabilityGetInfection;  // Текущая вероятность удачной попытки заразиться
+    private final float probabilityInfection;     // Текущая вероятность удачной попытки заразить
+    private final float probabilityGetInfection;  // Текущая вероятность удачной попытки заразиться
+    private final float probabilityGetInfHand;    // Текущая вероятность удачной попытки заразиться из-за контакта рук с лицом
     //=================
 
     // Конструктор
-    public Man(byte condition, float x, float y){
+    public Man(byte condition, boolean mask, boolean socDist,
+               float maskProtectionFor, float maskProtectionFrom,
+               float probabilityGetInfHand,
+               float x, float y){
         //
         this.condition = condition;
         this.x = x; this.y = y;
+        this.mask = mask; this.socDist = socDist;
+        infectHand = false;
+
         vectorDirection = new float[2];
         vectorDirection[0] = (float)Math.random()*2-1;
         vectorDirection[1] = (float)Math.random()*2-1;
-        //===== Время
-        timeRecovery = 1000;
-        timeSneeze = 100;
-        timeChangeDirect = 1;
         //===== Вероятности
-        probabilityInfection = 1.0f;
-        probabilityGetInfection = 1.0f;
+        if (mask){
+            probabilityInfection = 1.0f-maskProtectionFor;
+            probabilityGetInfection = 1.0f-maskProtectionFrom;
+        }
+        else{
+            probabilityInfection = 1.0f;
+            probabilityGetInfection = 1.0f;
+        }
+        this.probabilityGetInfHand = probabilityGetInfHand;
     }
 
     // Поменять направление движения
-    public void setVectorDirection(float x, float y){
+    private void setVectorDirection(float x, float y){
         vectorDirection[0] = x;
         vectorDirection[1] = y;
     }
@@ -57,33 +77,103 @@ public class Man {
 
     // Проверка таймеров
     private void timeCheck(Simulation sim){
-        //==== Время для выздоровления и чихания
-        if (condition == 1){
-            // Время для чихания
-            if (timeSneeze == 0){
-                contact(sim);
-                timeSneeze = (int)(Math.random()*(sim.timeSneeze_b-sim.timeSneeze_a+1)+sim.timeSneeze_a);
-            }
-            else {
-                timeSneeze--;
-            }
-            // Время для выздоровления
-            if (timeRecovery == 0) {
-                condition = 2;
-                sim.setAmountCond(0,0,-1,0,1,0);
-            }
-            else {
-                timeRecovery--;
-            }
+        //===== Таймеры для смены состояний
+        switch (condition){
+            case 0: // Здоров
+                // Таймер для мытья/дезинфекции рук
+                if (timeWash == 0){
+                    infectHand = false;
+                    timeWash = (int)(Math.random()*(sim.timeWash_b-sim.timeWash_a+1)+sim.timeWash_a);
+                }
+                else{
+                    timeWash--;
+                }
+
+                // Таймер для "трогания" лица
+                if (timeHandToFaceContact == 0){
+                    // Грязные ли руки
+                    if (infectHand){
+                        if (Math.random() <= probabilityGetInfHand){
+                            condition = 1;
+                            timeInfInc = (int)(Math.random()*(sim.timeInfInc_b-sim.timeInfInc_a+1)+sim.timeInfInc_a);
+                            sim.setAmountCond(-1,1,0,0,0,0);
+                        }
+                    }
+                }
+                else{
+                    timeHandToFaceContact--;
+                }
+                break;
+
+            case 1: // Инфицирован. В инкубационном периоде
+                // Таймер для выхода из инкубационного периода
+                if (timeInfInc == 0){
+                    // Шанс на то, чтобы стать бессимптомным больным
+                    if (Math.random() <= sim.probabilityNotSymp){
+                        // стал бессимптомным
+                        condition = 5;
+                        sim.setAmountCond(0,-1,0,1,0,0);
+                    }
+                    else{
+                        // стал симптомным
+                        condition = 2;
+                        sim.setAmountCond(0,-1,1,0,0,0);
+                    }
+                    // время для выздоровления
+                    timeRecovery = (int)(Math.random()*(sim.timeRecovery_b-sim.timeRecovery_a+1)+sim.timeRecovery_a);
+                }
+                else{
+                    timeInfInc--;
+                }
+                break;
+
+            case 2, 5: // Инфицирован. В клиническом периоде или без проявления симптомов
+                // Таймер чихания
+                if (timeSneeze == 0){
+                    contact(sim);
+                    timeSneeze = (int)(Math.random()*(sim.timeSneeze_b-sim.timeSneeze_a+1)+sim.timeSneeze_a);
+                }
+                else{
+                    timeSneeze--;
+                }
+
+                // Таймер выздоровления
+                if (timeRecovery == 0){
+
+                    // шанс умереть
+                    if (condition != 5 && Math.random() <= sim.probabilityDied){
+                        // умир
+                        condition = 4;
+                        sim.setAmountCond(0,0,-1,0,0,1);
+                    }
+                    else{
+                        // выздоровел
+                        // проверка для правильности ведения статистики
+                        if (condition == 2){
+                            sim.setAmountCond(0,0,-1,0,1,0);
+                        }
+                        else {
+                            sim.setAmountCond(0,0,0,-1,1,0);
+                        }
+                        condition = 3;
+                    }
+
+                }
+                else{
+                    timeRecovery--;
+                }
+                break;
         }
-        //===== Время для смены направления
-        if (timeChangeDirect != 0){
+
+        //===== Время для смены направления движения
+        if (timeChangeDirect == 0){
+            timeChangeDirect = (int)(Math.random()*(sim.timeChangeDirect_b-sim.timeChangeDirect_a+1)+sim.timeChangeDirect_a);
+        }
+        else{
             setVectorDirection((float)Math.random()*2-1,(float)Math.random()*2-1);
             timeChangeDirect--;
         }
-        else{
-            timeChangeDirect = (int)(Math.random()*(sim.timeChangeDirect_b-sim.timeChangeDirect_a+1)+sim.timeChangeDirect_a);
-        }
+
     }
 
     // Заставляем человека делать свои делишки
@@ -96,8 +186,8 @@ public class Man {
     }
 
     //============= Время
-    public void setTimeRecovery(int timeRecovery){
-        this.timeRecovery=timeRecovery;
+    public void setTimeInfInc(int timeInfInc){
+        this.timeInfInc=timeInfInc;
     }
 
     //============= Вероятности
--- src/main/Simulation.java
+++ src/main/Simulation.java
@@ -8,82 +8,168 @@ public class Simulation {
     private long iter = 0;            // номер текущей итерации
     //========== Настройки карты/симуляции
     private float sizeX, sizeY; // размеры карты по х и у
-    private int amountMans;     // количество людей на карте при старте симуляции
-    private float radiusMan;         // размер человека
-    private float radiusManSqr;      // размер человека (в квадрате)
-    //========== Настройки человеков
-    // интервалы для рандома
-    public final int timeRecovery_a, timeRecovery_b;        // для выздоровления
-    public final int timeSneeze_a, timeSneeze_b;            // для чихания
-    public final int timeChangeDirect_a, timeChangeDirect_b;// для смены направления движения
-    //========== Настройки болезни
-    private float radiusInf;        // радиус заражения
-    private float radiusInfSqr;     // радиус заражения (в квадрате)
+    public final int amountMans;     // количество людей на карте при старте симуляции
+    public final float radiusMan;    // размер человека // для того, чтобы не накладывались друг на друга
+    public final float radiusManSqr; // размер человека (в квадрате)
 
-    private float radiusSoc;        // радиус социального дистанцирования
-    private float radiusSocSqr;     // радиус социального дистанцирования (в квадрате)
+    public final int amountMask;     // Количество людей с масками
+    public final int amountSocDist;  // Количество людей соблюдающих соц. дистанцию
+    public final int amountWashHands;    // Количество людей моющих/дезинфицирующих руки
+    public final float radiusSoc;        // радиус социального дистанцирования
+    public final float radiusSocSqr;     // радиус социального дистанцирования (в квадрате)
+
+    //========== Настройки болезни
+    public final float maskProtectionFor;    // Эффективность защиты маски в сторону заражения кого-то
+    public final float maskProtectionFrom;   // Эффективность защиты маски в сторону заразиться от кого-то
+    public final float radiusInf;        // радиус заражения
+    public final float radiusInfSqr;     // радиус заражения (в квадрате)
+
+    // Время
+    public final int timeSneeze_a, timeSneeze_b;            // Между чиханиями
+    public final int timeHandToFaceContact_a, timeHandToFaceContact_b; // Между контактом рук с лицом
+    public final int timeWash_a, timeWash_b;                // Между мытьем/дезинфекцией рук
+    public final int timeChangeDirect_a, timeChangeDirect_b;// Между сменой направления движения
+
+    public final int timeInfInc_a, timeInfInc_b;        // Длительность инкубационного периода
+    public final int timeRecovery_a, timeRecovery_b;        // для выздоровления
+
+    // Вероятности
+    public final float probabilityNotSymp;          // Вероятность стать бессимптомным больным
+    public final float probabilityDied;             // Вероятность умереть из-за болезни
+    public final float probabilityInfHand;          // Вероятность заразиться от контакта рук с лицом
+
     //========== Статистика
     private long stContacts = 0;    // количество контактов за сессию
     private int iterFinal = 0;      // номер итерации, на которой закончились изменения
     // Количество здоровых,инфицированных в инкубац. периоде, инфиц. с симптомами, инфиц. без симптомов
     // выздоровевших, умерших в текущий момент времени
-    private int amountZd, amountInfInc, amountInfSymp, amountInfNotSymp, amountVzd, amountDied;
+    private int amountZd, amountInfInc, amountInfSymp,
+                amountInfNotSymp, amountVzd, amountDied;
     //=====================
 
 
     // Конструктор
-    public Simulation(float sizeX, float sizeY, int amountMans, int amountInf,
-                      int timeRecovery_a, int timeRecovery_b, int timeChangeDirect_a, int timeChangeDirect_b, int timeSneeze_a, int timeSneeze_b
+    public Simulation(float sizeX, float sizeY,
+                      // Количество людей в состояниях
+                      int amountZd, int amountInfInc, int amountInfSymp,
+                      int amountInfNotSymp, int amountVzd, int amountDied,
+
+                      int amountMask, int amountSocDist, int amountWashHands,
+                      // Время
+                      int timeSneeze_a, int timeSneeze_b,
+                      int timeHandToFaceContact_a, int timeHandToFaceContact_b,
+                      int timeWash_a, int timeWash_b,
+                      int timeChangeDirect_a, int timeChangeDirect_b,
+                      int timeInfInc_a, int timeInfInc_b,
+                      int timeRecovery_a, int timeRecovery_b,
+                      // Вероятность
+                      float probabilityNotSymp, float probabilityDied,
+                      float probabilityInfHand,
+                      float maskProtectionFor, float maskProtectionFrom,
+
+                      float radiusSoc, float radiusMan, float radiusInf
+
     ){
+
         people = new ArrayList<>();
         //===== Настройки карты, симуляции
         this.sizeX = sizeX; this.sizeY = sizeY;
-        this.amountMans = amountMans+amountInf;
-        amountZd = amountMans;
-        amountInfInc = 0;
-        amountInfSymp = amountInf;
-        amountInfNotSymp = 0;
-        amountVzd = 0;
-        amountDied = 0;
+        this.amountMans = amountZd+amountInfInc+amountInfSymp+amountInfNotSymp+amountVzd+amountDied;
+        //===== Количество людей в состояниях...
+        this.amountZd = amountZd;
+        this.amountInfInc = amountInfInc;
+        this.amountInfSymp = amountInfSymp;
+        this.amountInfNotSymp = amountInfNotSymp;
+        this.amountVzd = amountVzd;
+        this.amountDied = amountDied;
 
-        radiusMan = 0.2f;
+        this.amountMask = amountMask;
+        this.amountWashHands = amountWashHands;
+        this.amountSocDist = amountSocDist;
+        this.radiusMan = radiusMan;
         radiusManSqr = (float) Math.pow(radiusMan,2);
         //===== Настройки болезни
-        radiusInf = 1.5f;
+        this.radiusInf = radiusInf;
         radiusInfSqr = (float) Math.pow(radiusInf,2);
-        //===== Настройки человека
-        this.timeRecovery_a = timeRecovery_a;
-        this.timeRecovery_b = timeRecovery_b;
-        this.timeChangeDirect_a = timeChangeDirect_a;
-        this.timeChangeDirect_b = timeChangeDirect_b;
+
+        this.radiusSoc = radiusSoc;
+        radiusSocSqr = (float) Math.pow(radiusSoc,2);
+        // Настройки времени
         this.timeSneeze_a = timeSneeze_a;
         this.timeSneeze_b = timeSneeze_b;
+        this.timeHandToFaceContact_a = timeHandToFaceContact_a;
+        this.timeHandToFaceContact_b = timeHandToFaceContact_b;
+        this.timeWash_a = timeWash_a;
+        this.timeWash_b = timeWash_b;
+        this.timeInfInc_a = timeInfInc_a;
+        this.timeInfInc_b = timeInfInc_b;
+        this.timeChangeDirect_a = timeChangeDirect_a;
+        this.timeChangeDirect_b = timeChangeDirect_b;
+        this.timeRecovery_a = timeRecovery_a;
+        this.timeRecovery_b = timeRecovery_b;
+
+        // Вероятности
+        this.probabilityDied = probabilityDied;
+        this.probabilityNotSymp = probabilityNotSymp;
+        this.probabilityInfHand = probabilityInfHand;
+        this.maskProtectionFor = maskProtectionFor;
+        this.maskProtectionFrom = maskProtectionFrom;
 
         //=== заполнение списка людей на карте случайным образом
         float rx,ry;  // Рандомные координаты
         boolean f;  // Флаг поиска свободного места
-        // Заполняем инфицированных
-        for (int i = 0; i < amountInf; ++i) {
+        // Заполняем список людей
+        int zd = amountZd, infInc = amountInfInc, infSymp = amountInfSymp,
+                infNotSymp = amountInfNotSymp, vzd = amountVzd, died = amountDied;
+
+        for (int i = 0; i < amountMans; ++i) {
             // Поиск свободного места
             do{
                 rx = (float)Math.random() * sizeX;
                 ry = (float)Math.random() * sizeY;
                 f = this.checkPlace(rx,ry);
             } while (!f);
+
+            // выбор состояния
+            byte cond = 0;
+            if (zd != 0){
+                zd--;
+            }
+            else{
+                if (infInc != 0){
+                    cond = 1;
+                    infInc--;
+                }
+                else{
+                    if (infSymp != 0){
+                        cond = 2;
+                        infSymp--;
+                    }
+                    else{
+                        if (vzd != 0){
+                            cond = 3;
+                            vzd--;
+                        }
+                        else{
+                            if (died != 0){
+                                cond = 4;
+                                died--;
+                            }
+                            else {
+                                if (infNotSymp != 0){
+                                    cond = 5;
+                                    infNotSymp--;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
             // Создание человечека
-            people.add(new Man((byte)1,rx,ry));
+            people.add(new Man(cond, false,false,maskProtectionFor,maskProtectionFrom,probabilityInfHand,rx,ry));
         }
-        // Заполняем здоровых
-        for (int i = 0; i < amountMans; ++i) {
-            // Поиск свободного места
-            do{
-                rx = (float) (Math.random() * sizeX);
-                ry = (float) (Math.random() * sizeY);
-                f = this.checkPlace(rx,ry);
-            } while (!f);
-            // Создание человечека
-            people.add(new Man((byte)0,rx,ry));
-        }
+
     }
 
     // Проверка на возможность перемещения бойчика по карте
@@ -109,27 +195,27 @@ public class Simulation {
     // Произвести контакт между "man" и ближайшими людьми
     // только если "man" инфицирован
     public void makeContact(Man man){
-        if (man.getCondition() == 1){
-            // Поиск людей находящихся рядом с "man"
-            for (Man temp:people) {
-                // Проверка на возможность контакта
-                if ((Math.pow(man.getX() - temp.getX(), 2) + Math.pow(man.getY() - temp.getY(), 2)) < radiusInfSqr){
-                    stContacts++;  // статистика
-                    // Если человек здоровый
-                    if (temp.getCondition() == 0)
-                        // если вероятность на его стороне
-                        if (Math.random() < (man.getProbabilityInfection() * temp.getProbabilityGetInfection())) {
-                            // то заражает
-                            temp.setCondition((byte) 1);
-                            // устанавливаем время выздоровления
-                            temp.setTimeRecovery((int)(Math.random()*(timeRecovery_b-timeRecovery_a+1)+timeRecovery_a));
-                            // статистика
-                            setAmountCond(-1,0,1,0,0,0);
-                        }
-                }
-            }
 
+        // Поиск людей находящихся рядом с "man"
+        for (Man temp:people) {
+            // Проверка на возможность контакта
+            if ((Math.pow(man.getX() - temp.getX(), 2) + Math.pow(man.getY() - temp.getY(), 2)) < radiusInfSqr){
+                stContacts++;  // статистика
+                // Если человек здоровый
+                if (temp.getCondition() == 0)
+                    // если вероятность на его стороне
+                    if (Math.random() < (man.getProbabilityInfection() * temp.getProbabilityGetInfection())) {
+                        // то заражает
+                        temp.setCondition((byte) 2);
+                        // устанавливаем время выздоровления
+                        temp.setTimeInfInc((int)(Math.random()*(timeInfInc_b-timeInfInc_a+1)+timeInfInc_a));
+                        // статистика
+                        setAmountCond(-1,0,1,0,0,0);
+                    }
+            }
         }
+
+
     }
 
     // Заводим моторчик нашей мапы
@@ -151,9 +237,16 @@ public class Simulation {
         System.out.println("Выздоровевших: "+amountVzd);
         System.out.println("Смертей: "+amountDied);
     }
+
+    public void printInfoMans(){
+        for (Man man:people){
+            System.out.println(man.getCondition()+" | "+man.getX()+" "+man.getY());
+        }
+    }
     //======== Статистика
     // изменение статистики количества людей в различных состояниях
-    public void setAmountCond(int diffZd, int diffInfInc, int diffInfSymp, int diffInfNotSymp, int diffVzd, int diffDied){
+    public void setAmountCond(int diffZd, int diffInfInc, int diffInfSymp,
+                              int diffInfNotSymp, int diffVzd, int diffDied){
         if ((diffZd + diffInfInc + diffInfSymp + diffInfNotSymp + diffVzd + diffDied) == 0){
             amountZd+=diffZd;
             amountInfInc+=diffInfInc;
